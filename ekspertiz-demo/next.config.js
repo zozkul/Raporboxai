1b096416e9583f75b746161db004d4705de014c0
zozkul <59560794+zozkul@users.noreply.github.com>
2026-03-04 12:53:51 +0300

    Update next.config.js

--- ekspertiz-demo/next.config.js
+++ ekspertiz-demo/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
     },
   },
 }
